1d3483b0adf9b3ed393bf790be40fe3dfbb54e6b
mohit03nandan <mohitnandan81825@gmail.com>
2022-12-21 06:45:26 +0530

    added database model

---
+++ model/schema.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+
+
+    const userSchema = new mongoose.Schema({ 
+      
+            username: {
+                type: String,
+                required: true,
+            },
+            
+            password: {
+                type:string,
+                required: true,
+            },
+
+            createdAt: Date,
+            updatedAt: Date
+            
+    });
+
+    
+    const apilistSchema = new mongoose.Schema({
+        apiname: {
+            type: String,
+            required: true,
+        },
+        description: { 
+            type: String.apply, 
+            required: true,
+         },
+        imageurl: { 
+            type: String,
+            required: true,
+            },
+        apilink: { 
+            type: String, 
+            required: true 
+        },
+        createdAt: Date,
+        updatedAt: Date
+       
+    });
+    
+    const user = mongoose.model('user', userSchema);
+    const api = mongoose.model('api', apilistSchema);
+    
+    var my_schemas = {
+       "user": user,
+       "api": api,
+    };
+
+module.exports = my_schemas;
